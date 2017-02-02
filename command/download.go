ece9593ccffbdb3842dab01c23700c2a94c0fdcc
Boris Mattijssen <borismattijssen@gmail.com>
2017-02-02 11:59:13 +0100

    Add download functionality

--- command/download.go
+++ command/download.go
@@ -1,10 +1,13 @@
 package command
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/mitchellh/cli"
+	"github.com/nerdalize/nerd/nerd"
+	"github.com/nerdalize/nerd/nerd/data"
 )
 
 //DownloadOpts describes command options
@@ -25,7 +28,7 @@ func DownloadFactory() func() (cmd cli.Command, err error) {
 		command: &command{
 			help:     "",
 			synopsis: "fetch the output of a task from cloud storage",
-			parser:   flags.NewNamedParser("nerd upload", flags.Default),
+			parser:   flags.NewNamedParser("nerd download <dataset> <output-dir>", flags.Default),
 			ui: &cli.BasicUi{
 				Reader: os.Stdin,
 				Writer: os.Stderr,
@@ -48,5 +51,34 @@ func DownloadFactory() func() (cmd cli.Command, err error) {
 
 //DoRun is called by run and allows an error to be returned
 func (cmd *Download) DoRun(args []string) (err error) {
+	if len(args) < 2 {
+		return fmt.Errorf("not enough arguments, see --help")
+	}
+
+	dataset := args[0]
+	outputDir := args[1]
+
+	fi, err := os.Stat(outputDir)
+	if err != nil {
+		return fmt.Errorf("failed to inspect '%s': %v", outputDir, err)
+	} else if !fi.IsDir() {
+		return fmt.Errorf("provided path '%s' is not a directory", outputDir)
+	}
+
+	awsCreds, err := nerd.GetCurrentUser().GetAWSCredentials()
+	if err != nil {
+		return fmt.Errorf("could not get AWS credentials: %v", err)
+	}
+
+	client, err := data.NewClient(awsCreds)
+	if err != nil {
+		return fmt.Errorf("could not create data client: %v", err)
+	}
+
+	err = client.DownloadFiles(dataset, outputDir, &stdoutkw{}, 64)
+	if err != nil {
+		return fmt.Errorf("could not download files: %v", err)
+	}
+
 	return nil
 }
--- nerd/data/data.go
+++ nerd/data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
@@ -12,6 +13,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/nerdalize/nerd/nerd"
 )
+
+const DirectoryPermissions = 0755
 
 type KeyWriter interface {
 	Write(k string) error
@@ -98,6 +101,106 @@ func (client *Client) UploadFiles(files []string, dataset string, kw KeyWriter, 
 		}
 
 		err := kw.Write(it.filePath)
+		if err != nil {
+			return fmt.Errorf("failed to write key: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (client *Client) DownloadFile(key string, outDir string) error {
+	base := filepath.Dir(path.Join(outDir, key))
+	err := os.MkdirAll(base, DirectoryPermissions)
+	if err != nil {
+		return fmt.Errorf("failed to create path '%v': %v", base, err)
+	}
+	outFile, err := os.Create(path.Join(outDir, key))
+	defer outFile.Close()
+	if err != nil {
+		return fmt.Errorf("failed to create local file '%v': %v", path.Join(outDir, key), err)
+	}
+
+	svc := s3.New(client.Session)
+	params := &s3.GetObjectInput{
+		Bucket: aws.String(nerd.GetCurrentUser().AWSBucket), // Required
+		Key:    aws.String(key),                             // Required
+	}
+	resp, err := svc.GetObject(params)
+
+	if err != nil {
+		return fmt.Errorf("failed to download '%v': %v", key, err)
+	}
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to write output to '%v': %v", path.Join(outDir, key), err)
+	}
+
+	return nil
+}
+
+func (client *Client) ListDataset(dataset string) (keys []string, err error) {
+	svc := s3.New(client.Session)
+
+	params := &s3.ListObjectsInput{
+		Bucket: aws.String(nerd.GetCurrentUser().AWSBucket), // Required
+		Prefix: aws.String(dataset),
+	}
+	resp, err := svc.ListObjects(params)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list dataset '%v': %v", dataset, err)
+	}
+
+	for _, object := range resp.Contents {
+		keys = append(keys, aws.StringValue(object.Key))
+	}
+
+	return
+}
+
+func (client *Client) DownloadFiles(dataset string, outDir string, kw KeyWriter, concurrency int) error {
+	keys, err := client.ListDataset(dataset)
+	if err != nil {
+		return err
+	}
+
+	type item struct {
+		key    string
+		outDir string
+		resCh  chan bool
+		err    error
+	}
+
+	work := func(it *item) {
+		it.err = client.DownloadFile(it.key, it.outDir)
+		it.resCh <- true
+	}
+
+	itemCh := make(chan *item, concurrency)
+	go func() {
+		defer close(itemCh)
+		for i := 0; i < len(keys); i++ {
+			it := &item{
+				key:    keys[i],
+				outDir: outDir,
+				resCh:  make(chan bool),
+			}
+
+			go work(it)  //create work
+			itemCh <- it //send to fan-in thread for syncing results
+		}
+	}()
+
+	//fan-in
+	for it := range itemCh {
+		<-it.resCh
+		if it.err != nil {
+			return fmt.Errorf("failed to download '%v': %v", it.key, it.err)
+		}
+
+		err := kw.Write(path.Join(outDir, it.key))
 		if err != nil {
 			return fmt.Errorf("failed to write key: %v", err)
 		}
