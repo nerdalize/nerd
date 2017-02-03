d3a19cd5d9c45289892d1676520974ba7b139254
advanderveer <advanderveer@gmail.com>
2017-02-03 14:10:38 +0100

    added a status command that lists tasks

--- command/status.go
+++ command/status.go
@@ -1,11 +1,8 @@
 package command
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
 
@@ -14,34 +11,34 @@ import (
 	"github.com/nerdalize/nerd/nerd"
 )
 
-//RunOpts describes command options
-type RunOpts struct {
+//StatusOpts describes command options
+type StatusOpts struct {
 	*NerdAPIOpts
 }
 
-//Run command
-type Run struct {
+//Status command
+type Status struct {
 	*command
 
 	ui     cli.Ui
-	opts   *RunOpts
+	opts   *StatusOpts
 	parser *flags.Parser
 }
 
-//RunFactory returns a factory method for the join command
-func RunFactory() func() (cmd cli.Command, err error) {
-	cmd := &Run{
+//StatusFactory returns a factory method for the join command
+func StatusFactory() func() (cmd cli.Command, err error) {
+	cmd := &Status{
 		command: &command{
 			help:     "",
-			synopsis: "create a new compute task for a dataset",
-			parser:   flags.NewNamedParser("nerd run <image> <dataset>", flags.Default),
+			synopsis: "show the status of all queued tasks",
+			parser:   flags.NewNamedParser("nerd status", flags.Default),
 			ui: &cli.BasicUi{
 				Reader: os.Stdin,
 				Writer: os.Stderr,
 			},
 		},
 
-		opts: &RunOpts{},
+		opts: &StatusOpts{},
 	}
 
 	cmd.runFunc = cmd.DoRun
@@ -56,34 +53,18 @@ func RunFactory() func() (cmd cli.Command, err error) {
 }
 
 //DoRun is called by run and allows an error to be returned
-func (cmd *Run) DoRun(args []string) (err error) {
-	if len(args) < 2 {
-		return fmt.Errorf("not enough arguments, see --help")
-	}
+func (cmd *Status) DoRun(args []string) (err error) {
 
 	loc, err := cmd.opts.URL("/tasks")
 	if err != nil {
 		return fmt.Errorf("failed to create API url from cli options: %+v", err)
 	}
 
-	log.Printf("submitting task to %s", loc)
-	body := bytes.NewBuffer(nil)
-	enc := json.NewEncoder(body)
-	err = enc.Encode(&nerd.Task{
-		Image:   args[0],
-		Dataset: args[1],
-		Args:    args[2:],
-	})
-	if err != nil {
-		return fmt.Errorf("failed to encode provided task definition: %v", err)
-	}
-
-	req, err := http.NewRequest("POST", loc.String(), body)
+	req, err := http.NewRequest("GET", loc.String(), nil)
 	if err != nil {
 		return fmt.Errorf("failed to create API request: %+v", err)
 	}
 
-	//@TODO abstract into a default http client
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return fmt.Errorf("API request '%s %s' failed: %v", req.Method, loc, err)
@@ -94,10 +75,15 @@ func (cmd *Run) DoRun(args []string) (err error) {
 		return fmt.Errorf("API request '%s %s' returned unexpected status from API: %v", req.Method, loc, resp.Status)
 	}
 
-	//@TODO find a more user friendly way of returning info from the API
-	_, err = io.Copy(os.Stderr, resp.Body)
+	tasks := []*nerd.Task{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&tasks)
 	if err != nil {
-		return fmt.Errorf("failed to output API response: %v", err)
+		return fmt.Errorf("failed to decode: %v", err)
+	}
+
+	for _, t := range tasks {
+		fmt.Printf("%s (%s@%s): %s\n", t.ID, t.Image, t.Dataset, t.Status)
 	}
 
 	return nil
--- nerd/task.go
+++ nerd/task.go
@@ -2,9 +2,11 @@ package nerd
 
 //Task describes a piece of workload
 type Task struct {
+	ID      string   `json:"id"`
 	Image   string   `json:"image"`
 	Dataset string   `json:"dataset"`
 	Args    []string `json:"args"`
+	Status  string   `json:"status"`
 }
 
 //TaskStatus is used to update the status of a task
