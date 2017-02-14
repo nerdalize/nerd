ff70ffd36b49bf95d7cf6f96b468d674539c74e0
advanderveer <advanderveer@gmail.com>
2017-02-14 11:46:55 +0100

    example integration test

---
+++ utils_test.go
@@ -0,0 +1,44 @@
+package main_test
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"runtime"
+	"testing"
+)
+
+// this miniature framework is from https://github.com/benbjohnson/testing, to quote: "No, seriously. They're tiny functions. Just copy them."
+
+// assert fails the test if the condition is false.
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// ok fails the test if an err is not nil.
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
+}
+
+func assertbuf(tb testing.TB, buf *bytes.Buffer, pattern string) {
+	assert(tb, regexp.MustCompile(pattern).MatchString(buf.String()), "expected '%s' to match pattern '%s'", buf.String(), pattern)
+}
