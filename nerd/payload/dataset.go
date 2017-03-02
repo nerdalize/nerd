56b20eef907ce5bc4382d27cd1b5a3290e2bcf93
advanderveer <advanderveer@gmail.com>
2017-03-02 12:20:55 +0100

    removed key structure

--- nerd/payload/dataset.go
+++ nerd/payload/dataset.go
@@ -6,13 +6,11 @@ type DatasetCreateInput struct{}
 //DatasetCreateOutput is returned from creating a dataset
 type DatasetCreateOutput struct {
 	Dataset
-	DatasetDetails
 }
 
 //DatasetDescribeOutput is returned from a specific dataset
 type DatasetDescribeOutput struct {
 	Dataset
-	DatasetDetails
 }
 
 //DatasetListOutput is returned from the dataset listing
@@ -20,15 +18,10 @@ type DatasetListOutput struct {
 	Datasets []*Dataset `json:"datasets"`
 }
 
-//DatasetDetails holds detailed information
-type DatasetDetails struct {
-	Root string   `json:"root"`
-	Keys [][]byte `json:"keys"`
-}
-
 //Dataset is a dataset in the list output
 type Dataset struct {
 	ProjectID string `json:"project_id"`
 	DatasetID string `json:"dataset_id"`
 	Bucket    string `json:"bucket"`
+	Root      string `json:"root"`
 }
