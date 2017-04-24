1fc3d7af401d24ea5291c282af3f3245d23a35ad
advanderveer <advanderveer@gmail.com>
2017-04-24 12:26:19 +0200

    proposal

--- proposal_v2_client.go
+++ proposal_v2_client.go
@@ -5,3 +5,23 @@ type Client struct {
 	Nerd NerdClient
 	AWS  AWSClient
 }
+
+type NerdClient struct{}
+
+type AuthClient struct{}
+
+type AWSStorageBackend interface {
+	DownloadObject()
+	UploadObject()
+}
+
+type AWSQueueBackend interface {
+	ReceiveMessages() [][]byte
+}
+
+type AWSBackend interface {
+	AWSQueueBackend
+	AWSStorageBackend
+}
+
+func NewRealAWSBackend(sess aws.Session)
