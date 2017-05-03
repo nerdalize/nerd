a202b6c62ccec6f8cf63e3c732cfa7a66a48d884
advanderveer <advanderveer@gmail.com>
2017-05-03 10:45:34 +0200

    added list, describe dataset

--- nerd/client/batch/v1/dataset.go
+++ nerd/client/batch/v1/dataset.go
@@ -9,17 +9,27 @@ import (
 //ClientDatasetInterface is an interface so client dataset calls can be mocked.
 type ClientDatasetInterface interface {
 	CreateDataset(projectID string) (output *v1payload.CreateDatasetOutput, err error)
+	ListDatasets(projectID string) (output *v1payload.ListDatasetsOutput, err error)
 	DescribeDataset(projectID, id string) (output *v1payload.DescribeDatasetOutput, err error)
 }
 
 //CreateDataset creates a new dataset.
 func (c *Client) CreateDataset(projectID string) (output *v1payload.CreateDatasetOutput, err error) {
+	input := &v1payload.CreateDatasetInput{}
 	output = &v1payload.CreateDatasetOutput{}
-	return output, c.doRequest(http.MethodPost, createPath(projectID, datasetEndpoint), nil, output)
+	return output, c.doRequest(http.MethodPost, createPath(projectID, datasetEndpoint), input, output)
 }
 
 //DescribeDataset gets a dataset by ID.
 func (c *Client) DescribeDataset(projectID, id string) (output *v1payload.DescribeDatasetOutput, err error) {
+	input := &v1payload.DescribeDatasetInput{}
 	output = &v1payload.DescribeDatasetOutput{}
-	return output, c.doRequest(http.MethodGet, createPath(projectID, datasetEndpoint, id), nil, output)
+	return output, c.doRequest(http.MethodGet, createPath(projectID, datasetEndpoint, id), input, output)
+}
+
+//ListDatasets gets a dataset by ID.
+func (c *Client) ListDatasets(projectID string) (output *v1payload.ListDatasetsOutput, err error) {
+	input := &v1payload.ListDatasetsInput{}
+	output = &v1payload.ListDatasetsOutput{}
+	return output, c.doRequest(http.MethodGet, createPath(projectID, datasetEndpoint), input, output)
 }
