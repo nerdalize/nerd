8e27d548a1e327a209ce26eb65491fa96238e0f0
advanderveer <advanderveer@gmail.com>
2017-05-19 10:54:33 +0200

    reduced start worker signature

--- nerd/client/batch/v1/worker.go
+++ nerd/client/batch/v1/worker.go
@@ -8,12 +8,12 @@ import (
 
 //ClientWorkerInterface is an interface for placement of project
 type ClientWorkerInterface interface {
-	StartWorker(projectID, host, token, capem string) (output *v1payload.StartWorkerOutput, err error)
+	StartWorker(projectID string) (output *v1payload.StartWorkerOutput, err error)
 	StopWorker(projectID string) (output *v1payload.StopWorkerOutput, err error)
 }
 
 //StartWorker will create queue
-func (c *Client) StartWorker(projectID, host, token, capem string) (output *v1payload.StartWorkerOutput, err error) {
+func (c *Client) StartWorker(projectID string) (output *v1payload.StartWorkerOutput, err error) {
 	output = &v1payload.StartWorkerOutput{}
 	input := &v1payload.StartWorkerInput{
 		ProjectID: projectID,
