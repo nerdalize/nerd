dc62f98305faf00709a94921b524259eaf452926
nov1n <robert@carosi.nl>
2017-06-07 16:19:43 +0200

    Add formatter to client

--- command/secret_list.go
+++ command/secret_list.go
@@ -29,7 +29,7 @@ func SecretListFactory() (cli.Command, error) {
 
 //DoRun is called by run and allows an error to be returned
 func (cmd *SecretList) DoRun(args []string) (err error) {
-	bclient, err := NewClient(cmd.config, cmd.session)
+	bclient, err := NewClient(cmd.config, cmd.session, cmd.outputter)
 	if err != nil {
 		HandleError(err)
 	}
