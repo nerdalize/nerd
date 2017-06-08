f839fc9344479ae8196247c511e137c537a1db9a
advanderveer <advanderveer@gmail.com>
2017-06-08 13:00:37 +0200

    added worker status and conditions

--- nerd/client/batch/v1/payload/worker.go
+++ nerd/client/batch/v1/payload/worker.go
@@ -1,6 +1,16 @@
 package v1payload
 
+import "time"
+
+//WorkerCondition describes the worker status at a point in time
+type WorkerCondition struct {
+	ProbeTime time.Time `json:"probe_time"`
+	Type      string    `json:"type"`
+}
+
 //WorkerSummary is a small version
 type WorkerSummary struct {
-	WorkerID string `json:"worker_id"`
+	WorkerID   string             `json:"worker_id"`
+	Status     string             `json:"status"`
+	Conditions []*WorkerCondition `json:"conditions"`
 }
