c53cd13fd1fe4f94e4a542b911a5afc6ed209db7
Dexter <dexter@dexterlabs.nl>
2017-07-11 13:54:16 +0200

    Added debug env var to enable debugging

--- command/opts.go
+++ command/opts.go
@@ -2,7 +2,7 @@ package command
 
 //OutputOpts are options that are related to CLI output.
 type OutputOpts struct {
-	Debug  func(bool)   `long:"debug" default:"false" optional:"true" optional-value:"true" description:"show debug output"`
+	Debug  func(bool)   `long:"debug" default:"false" optional:"true" optional-value:"true" description:"show debug output" env:"NERD_DEBUG"`
 	Output func(string) `long:"output" default:"pretty" description:"[pretty|raw|json]"`
 }
 
