944d084b5e805fba410b25c7b5cd6249ee336e7f
Emeline Gaulard <emeline.gaulard@epitech.eu>
2017-08-14 11:14:56 +0200

    remove useless fields that we'll get with the cli

--- nerd/client/batch/v1/billing_package.go
+++ nerd/client/batch/v1/billing_package.go
@@ -20,9 +20,7 @@ type ClientBillingPackageInterface interface {
 func (c *Client) CreateBillingPackage(projectID, billingPackageID, requestsCPU string) (output *v1payload.CreateBillingPackageOutput, err error) {
 	output = &v1payload.CreateBillingPackageOutput{}
 	input := &v1payload.CreateBillingPackageInput{
-		ProjectID:        projectID,
-		BillingPackageID: billingPackageID,
-		RequestsCPU:      requestsCPU,
+		RequestsCPU: requestsCPU,
 	}
 
 	return output, c.doRequest(http.MethodPost, createPath(projectID, billingPackagesEndpoint), input, output)
@@ -31,10 +29,7 @@ func (c *Client) CreateBillingPackage(projectID, billingPackageID, requestsCPU s
 // RemoveBillingPackage will delete a billing package from the precised project.
 func (c *Client) RemoveBillingPackage(projectID, billingPackageID string) (output *v1payload.RemoveBillingPackageOutput, err error) {
 	output = &v1payload.RemoveBillingPackageOutput{}
-	input := &v1payload.RemoveBillingPackageInput{
-		ProjectID:        projectID,
-		BillingPackageID: billingPackageID,
-	}
+	input := &v1payload.RemoveBillingPackageInput{}
 
 	return output, c.doRequest(http.MethodDelete, createPath(projectID, billingPackagesEndpoint, billingPackageID), input, output)
 }
@@ -42,9 +37,7 @@ func (c *Client) RemoveBillingPackage(projectID, billingPackageID string) (outpu
 // DeleteBillingPackage will delete a billing package with the provided .
 func (c *Client) DeleteBillingPackage(billingPackageID string) (output *v1payload.DeleteBillingPackageOutput, err error) {
 	output = &v1payload.DeleteBillingPackageOutput{}
-	input := &v1payload.DeleteBillingPackageInput{
-		BillingPackageID: billingPackageID,
-	}
+	input := &v1payload.DeleteBillingPackageInput{}
 
 	return output, c.doRequest(http.MethodDelete, createPath("", billingPackagesEndpoint, billingPackageID), input, output)
 }
@@ -52,31 +45,25 @@ func (c *Client) DeleteBillingPackage(billingPackageID string) (output *v1payloa
 // ListBillingPackages will return all billingPackages for a particular project if precised.
 func (c *Client) ListBillingPackages(projectID string) (output *v1payload.ListBillingPackagesOutput, err error) {
 	output = &v1payload.ListBillingPackagesOutput{}
-	input := &v1payload.ListBillingPackagesInput{
-		ProjectID: projectID,
-	}
+	input := &v1payload.ListBillingPackagesInput{}
 
 	return output, c.doRequest(http.MethodGet, createPath(projectID, billingPackagesEndpoint), input, output)
 }
 
 // DescribeBillingPackage returns detailed information of a billing package.
-func (c *Client) DescribeBillingPackage(projectID string) (output *v1payload.DescribeBillingPackageOutput, err error) {
+func (c *Client) DescribeBillingPackage(projectID, billingPackageID string) (output *v1payload.DescribeBillingPackageOutput, err error) {
 	output = &v1payload.DescribeBillingPackageOutput{}
-	input := &v1payload.DescribeBillingPackageInput{
-		ProjectID: projectID,
-	}
+	input := &v1payload.DescribeBillingPackageInput{}
 
-	return output, c.doRequest(http.MethodGet, createPath(projectID, billingPackagesEndpoint), input, output)
+	return output, c.doRequest(http.MethodGet, createPath(projectID, billingPackagesEndpoint, billingPackageID), input, output)
 }
 
 // UpdateBillingPackage returns a billing package with an updated cpu request.
 func (c *Client) UpdateBillingPackage(projectID, billingPackageID, requestsCPU, requestsMemory string) (output *v1payload.UpdateBillingPackageOutput, err error) {
 	output = &v1payload.UpdateBillingPackageOutput{}
 	input := &v1payload.UpdateBillingPackageInput{
-		ProjectID:        projectID,
-		BillingPackageID: billingPackageID,
-		RequestsCPU:      requestsCPU,
-		RequestsMemory:   requestsMemory,
+		RequestsCPU:    requestsCPU,
+		RequestsMemory: requestsMemory,
 	}
 
 	return output, c.doRequest(http.MethodPut, createPath(projectID, billingPackagesEndpoint, billingPackageID), input, output)
--- nerd/client/batch/v1/payload/billing_package.go
+++ nerd/client/batch/v1/payload/billing_package.go
@@ -3,8 +3,6 @@ package v1payload
 // CreateBillingPackageInput is the input for assigning a billing package to a project.
 // This results in the creation of a quota in the right namespace.
 type CreateBillingPackageInput struct {
-	ProjectID        string `json:"project_id" valid:"required"`
-	BillingPackageID string `json:"billing_package_id" valid:"required"`
 	RequestsCPU      string `json:"requests_cpu"`
 }
 
@@ -18,10 +16,8 @@ type CreateBillingPackageOutput struct {
 
 // UpdateBillingPackageInput is the input for updating the billing package capacity
 type UpdateBillingPackageInput struct {
-	ProjectID        string `json:"project_id" valid:"required"`
-	BillingPackageID string `json:"billing_package_id" valid:"required"`
-	RequestsCPU      string `json:"requests_cpu"`
-	RequestsMemory   string `json:"requests_memory"`
+	RequestsCPU    string `json:"requests_cpu"`
+	RequestsMemory string `json:"requests_memory"`
 }
 
 // UpdateBillingPackageOutput is the output for updating the billing package capacity
@@ -33,10 +29,8 @@ type UpdateBillingPackageOutput struct {
 }
 
 // DescribeBillingPackageInput is the input for describing a billing package.
-// As a billingPackage is not necessarily always assigned to a project, the ProjectID can be empty.
+// Parameters are fetched through the url
 type DescribeBillingPackageInput struct {
-	ProjectID        string `json:"project_id"`
-	BillingPackageID string `json:"billing_package_id" valid:"required"`
 }
 
 // DescribeBillingPackageOutput is the output from describing a billingPackage.
@@ -53,8 +47,6 @@ type DescribeBillingPackageOutput struct {
 
 // RemoveBillingPackageInput is the input for removing a billing package from a project
 type RemoveBillingPackageInput struct {
-	BillingPackageID string `json:"billing_package_id" valid:"required"`
-	ProjectID        string `json:"project_id" valid:"required"`
 }
 
 // RemoveBillingPackageOutput is the output from removing a billing package from a project
@@ -63,7 +55,6 @@ type RemoveBillingPackageOutput struct {
 
 // DeleteBillingPackageInput is the input for deleting a billing package
 type DeleteBillingPackageInput struct {
-	BillingPackageID string `json:"billing_package_id" valid:"required"`
 }
 
 // DeleteBillingPackageOutput is the output from deleting a billing package
@@ -77,9 +68,7 @@ type BillingPackageSummary struct {
 }
 
 // ListBillingPackagesInput is the input for listing billing packages.
-// The ProjectID can be empty, so this would results in a list of purchased billing packages.
 type ListBillingPackagesInput struct {
-	ProjectID string `json:"project_id" valid:"required"`
 }
 
 // ListBillingPackagesOutput is the output from listing billing packages of a project
