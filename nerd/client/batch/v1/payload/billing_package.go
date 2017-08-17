52ebb7776db861f5890d4547b4b28a697a6be45f
Emeline Gaulard <emeline.gaulard@epitech.eu>
2017-08-17 15:16:47 +0200

    back to cpu/memory types as strings

--- nerd/client/batch/v1/payload/billing_package.go
+++ nerd/client/batch/v1/payload/billing_package.go
@@ -1,7 +1,5 @@
 package v1payload
 
-import "k8s.io/apimachinery/pkg/api/resource"
-
 // CreateBillingPackageInput is the input for assigning a billing package to a project.
 // This results in the creation of a quota in the right namespace.
 type CreateBillingPackageInput struct {
@@ -12,24 +10,25 @@ type CreateBillingPackageInput struct {
 
 // CreateBillingPackageOutput is the output from assigning a billing package to a project.
 type CreateBillingPackageOutput struct {
-	ProjectID        string             `json:"project_id" valid:"required"`
-	BillingPackageID string             `json:"billing_package_id" valid:"required"`
-	RequestsCPU      *resource.Quantity `json:"requests_cpu"`
-	RequestsMemory   *resource.Quantity `json:"requests_memory"`
+	ProjectID        string `json:"project_id" valid:"required"`
+	BillingPackageID string `json:"billing_package_id" valid:"required"`
+	RequestsCPU      string `json:"requests_cpu"`
+	RequestsMemory   string `json:"requests_memory"`
 }
 
 // UpdateBillingPackageInput is the input for updating the billing package capacity
 type UpdateBillingPackageInput struct {
+	OnDemand       bool   `json:"on_demand"`
 	RequestsCPU    string `json:"requests_cpu"`
 	RequestsMemory string `json:"requests_memory"`
 }
 
 // UpdateBillingPackageOutput is the output for updating the billing package capacity
 type UpdateBillingPackageOutput struct {
-	ProjectID        string             `json:"project_id" valid:"required"`
-	BillingPackageID string             `json:"billing_package_id" valid:"required"`
-	RequestsCPU      *resource.Quantity `json:"requests_cpu"`
-	RequestsMemory   *resource.Quantity `json:"requests_memory"`
+	ProjectID        string `json:"project_id" valid:"required"`
+	BillingPackageID string `json:"billing_package_id" valid:"required"`
+	RequestsCPU      string `json:"requests_cpu"`
+	RequestsMemory   string `json:"requests_memory"`
 }
 
 // DescribeBillingPackageInput is the input for describing a billing package.
@@ -41,12 +40,12 @@ type DescribeBillingPackageInput struct {
 // Same as the DescribeBillingPackageInput, a billing package is not always
 // assigned to a project so the projectID can be empty.
 type DescribeBillingPackageOutput struct {
-	ProjectID        string             `json:"project_id"`
-	BillingPackageID string             `json:"billing_package_id" valid:"required"`
-	RequestsCPU      *resource.Quantity `json:"requests_cpu"`
-	RequestsMemory   *resource.Quantity `json:"requests_memory"`
-	UsedCPU          *resource.Quantity `json:"used_cpu"`
-	UsedMemory       *resource.Quantity `json:"used_memory"`
+	ProjectID        string `json:"project_id"`
+	BillingPackageID string `json:"billing_package_id" valid:"required"`
+	RequestsCPU      string `json:"requests_cpu"`
+	RequestsMemory   string `json:"requests_memory"`
+	UsedCPU          string `json:"used_cpu"`
+	UsedMemory       string `json:"used_memory"`
 }
 
 // RemoveBillingPackageInput is the input for removing a billing package from a project
