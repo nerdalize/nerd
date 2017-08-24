e80e8a0e4827404c4d7925f628f1895433b34c69
Emeline Gaulard <emeline.gaulard@epitech.eu>
2017-08-24 14:41:12 +0200

    update spec.json and opts printing format

--- command/opts.go
+++ command/opts.go
@@ -2,13 +2,13 @@ package command
 
 //OutputOpts are options that are related to CLI output.
 type OutputOpts struct {
-	Debug  func(bool)   `long:"debug" default:"false" optional:"true" optional-value:"true" description:"show debug output" env:"NERD_DEBUG"`
-	Output func(string) `long:"output" default:"pretty" description:"format of the output" choice:"pretty" choice:"raw" choice:"json"`
+	Debug  func(bool)   `long:"debug" default:"false" optional:"true" optional-value:"true" description:"Show debug output" env:"NERD_DEBUG"`
+	Output func(string) `long:"output" default:"pretty" description:"Format of the output" choice:"pretty" choice:"raw" choice:"json"`
 }
 
 //ConfOpts are the options related to config file and the way output is handled.
 type ConfOpts struct {
-	ConfigFile  func(string) `long:"config-file" default:"" default-mask:"" env:"NERD_CONFIG_FILE" description:"location of config file"`
-	SessionFile func(string) `long:"session-file" default:"" default-mask:"" env:"NERD_SESSION_FILE" description:"location of session file"`
+	ConfigFile  func(string) `long:"config-file" default:"" default-mask:"" env:"NERD_CONFIG_FILE" description:"Location of config file"`
+	SessionFile func(string) `long:"session-file" default:"" default-mask:"" env:"NERD_SESSION_FILE" description:"Location of session file"`
 	OutputOpts
 }
