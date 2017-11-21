126d9e53e3d25eaeff8bfcd80868c572676713a8
Emeline Gaulard <emeline.gaulard@gmail.com>
2017-11-21 11:44:10 +0100

    fix workload create

--- nerd/client/batch/v1/workload.go
+++ nerd/client/batch/v1/workload.go
@@ -8,14 +8,14 @@ import (
 
 //ClientWorkloadInterface is an interface so client workload calls can be mocked.
 type ClientWorkloadInterface interface {
-	CreateWorkload(projectID, image, inputDatasetID, pullSecret, computeUnits string, env map[string]string, instances int, useCuteur bool) (output *v1payload.CreateWorkloadOutput, err error)
+	CreateWorkload(projectID, image, inputDatasetID, pullSecret string, computeUnits uint64, env map[string]string, instances int, useCuteur bool) (output *v1payload.CreateWorkloadOutput, err error)
 	StopWorkload(projectID, workloadID string) (output *v1payload.StopWorkloadOutput, err error)
 	ListWorkloads(projectID string) (output *v1payload.ListWorkloadsOutput, err error)
 	DescribeWorkload(projectID, workloadID string) (output *v1payload.DescribeWorkloadOutput, err error)
 }
 
 //CreateWorkload will start a workload
-func (c *Client) CreateWorkload(projectID, image, inputDatasetID, pullSecret, computeUnits string, env map[string]string, nrOfWorkers int, useCuteur bool) (output *v1payload.CreateWorkloadOutput, err error) {
+func (c *Client) CreateWorkload(projectID, image, inputDatasetID, pullSecret string, computeUnits uint64, env map[string]string, nrOfWorkers int, useCuteur bool) (output *v1payload.CreateWorkloadOutput, err error) {
 	output = &v1payload.CreateWorkloadOutput{}
 	input := &v1payload.CreateWorkloadInput{
 		ProjectID:      projectID,
