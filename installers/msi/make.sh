0f26227f5e70cdbf457eb52a03bffd9708c95ced
advanderveer <advanderveer@gmail.com>
2018-03-02 09:50:34 +0100

    fixed installer version being parsed correctly

--- installers/msi/make.sh
+++ installers/msi/make.sh
@@ -2,7 +2,8 @@
 set -e
 
 cp $GOPATH/bin/nerd ./nerd.exe
-VERSION=$(cat ../../VERSION)
+VERSION=$(cat ../../VERSION | cut -f1 -d"-")
+
 sed -i "s/\sVersion=".*"/ Version=\"$VERSION\"/" Product.wxs
 
 $WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe nerd.wixproject
